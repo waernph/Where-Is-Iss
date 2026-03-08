b0f7993f5fe5bd463a2b3ad357dbd152634a06d6
Philip Waern <waernph@gmail.com>
2026-03-08 20:18:47 +0100

    working tracking of position over blue bg

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -2,11 +2,18 @@
 async function issApi() {
         let response = await fetch("http://api.open-notify.org/iss-now.json");
         let result = await response.json();
+        const map = document.querySelector("iframe");
+        const key = "359cb23abf673e47ca98b9a1c89ab8c7";
+        let endpointMap = "http://api.positionstack.com/v1/";
         const latitude = result['iss_position']['latitude'];
         const longitude = result['iss_position']['longitude'];
         document.getElementById("lat").innerText = "Latitude: " + latitude;
         document.getElementById("long").innerText = "Longitude: " + longitude;
-        console.log("Latitude: " + lat + " Longitude: " + long);
+        const issDot = document.querySelector("#iss")
+        issDot.style.left = (Number(longitude) + 180) + "px";
+        issDot.style.top = (90 - Number(latitude)) + "px";
+        console.log(typeof(latitude));
+        
         return result;
 }
 
