b3e069b7bc09b5052846af6de170e74de7f3aae0
Philip Waern <waernph@gmail.com>
2026-03-09 07:39:30 +0100

    Added zoom variable to zoom map and adjust position of iss

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,22 +1,23 @@
+let zoomAmount = 3;
 
 async function issApi() {
         let response = await fetch("http://api.open-notify.org/iss-now.json");
         let result = await response.json();
-        const map = document.querySelector("iframe");
-        const key = "359cb23abf673e47ca98b9a1c89ab8c7";
-        let endpointMap = "http://api.positionstack.com/v1/";
         const latitude = result['iss_position']['latitude'];
         const longitude = result['iss_position']['longitude'];
         document.getElementById("lat").innerText = "Latitude: " + latitude;
         document.getElementById("long").innerText = "Longitude: " + longitude;
+        const map = document.querySelector("img");
+        const background = document.querySelector(".map");
+        map.style.width = (360 * zoomAmount) + "px";
+        map.style.height = (180 * zoomAmount) + "px";
+        background.style.width = (360 * zoomAmount) + "px";
+        background.style.height = (180 * zoomAmount) + "px";
         const issDot = document.querySelector("#iss")
-        issDot.style.left = (Number(longitude) + 180) + "px";
-        issDot.style.top = (90 - Number(latitude)) + "px";
-        console.log(typeof(latitude));
-        
-        return result;
+        issDot.style.left = (zoomAmount * (Number(longitude) + 180)) + "px";
+        issDot.style.top = (zoomAmount * (90 - Number(latitude))) + "px";
 }
 
 setInterval(() => {
     issApi();
-}, 1500);
+}, 2000);
