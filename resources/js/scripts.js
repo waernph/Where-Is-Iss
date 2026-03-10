07518b9fc1d8804517ed38d4c2a1904170ea08c0
Philip Waern <waernph@gmail.com>
2026-03-10 19:22:04 +0100

    switched place on position and map in html. Change the order in get iss position-function in scripts.js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,22 +1,23 @@
 let zoomAmount = 3;
 
 async function issApi() {
-        let response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
-        let result = await response.json();
-        const latitude = result.latitude;
-        const longitude = result.longitude;
-        document.getElementById("lat").innerText = "Latitude: " + latitude;
-        document.getElementById("long").innerText = "Longitude: " + longitude;
-        const map = document.querySelector("img");
-        const background = document.querySelector(".map");
-        map.style.width = (360 * zoomAmount) + "px";
-        map.style.height = (180 * zoomAmount) + "px";
-        background.style.width = (360 * zoomAmount) + "px";
-        background.style.height = (180 * zoomAmount) + "px";
-        const issDot = document.querySelector("#iss")
-        issDot.style.left = (zoomAmount * (Number(longitude) + 180)) + "px";
-        issDot.style.top = (zoomAmount * (90 - Number(latitude))) + "px";
+    const map = document.querySelector("img");
+    const background = document.querySelector(".map");
+    map.style.width = (360 * zoomAmount) + "px";
+    map.style.height = (180 * zoomAmount) + "px";
+    background.style.width = (360 * zoomAmount) + "px";
+    background.style.height = (180 * zoomAmount) + "px";
+    let response = await fetch("https://api.wheretheiss.at/v1/satellites/25544");
+    let result = await response.json();
+    const latitude = result.latitude;
+    const longitude = result.longitude;
+    document.getElementById("lat").innerText = "Latitude: " + latitude;
+    document.getElementById("long").innerText = "Longitude: " + longitude;
+    const issDot = document.querySelector("#iss")
+    issDot.style.left = (zoomAmount * (Number(longitude) + 180)) + "px";
+    issDot.style.top = (zoomAmount * (90 - Number(latitude))) + "px";
 }
+issApi();
 
 setInterval(() => {
     issApi();
